e033bc470c5cf6770eb78d314cad79035de2d2e8
zhouyu <zhouyu@devzhou.cn>
2017-12-05 14:03:16 +0800

    first commit

---
+++ Server/Generate.cmd
@@ -0,0 +1,10 @@
+﻿setlocal
+
+cd /d %~dp0
+
+set TOOLS_PATH=.\tools\windows_x86
+set OUT_PUT=.\Service
+
+%TOOLS_PATH%\protoc.exe -I ./protos --csharp_out %OUT_PUT%  .\protos\sample.proto --grpc_out  %OUT_PUT%\ --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
+
+endlocal
---
+++ Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Sample;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Grpc.Core.Server
+            {
+                Services = { UserService.BindService(new SampleImpl()) },
+                Ports = { new ServerPort("localhost", 8080, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+            Console.WriteLine("server running on localhost:8080");
+            server.ShutdownTask.Wait();
+
+    }
+  }
+
+    class SampleImpl : UserService.UserServiceBase
+    {
+        public override Task<User> GetUser(NotFound request, ServerCallContext context)
+        {
+            return Task.FromResult(new User { Id = 1, Name = "陌上花开" });
+        }
+    }
+}
