e35f52077bf137bd0614516ada8bbc483d23f39f
zhouyu <zhouyu@devzhou.cn>
2017-12-06 10:26:21 +0800

    change project structure

---
+++ Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Grpc.Core;
+using Sample;
+
+namespace Client
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var chnanel = new Channel("localhost:8080", ChannelCredentials.Insecure);
+      var client = new UserService.UserServiceClient(chnanel);
+      var result = client.GetUser(new NotFound());
+      Console.WriteLine(result.ToString());
+    }
+  }
+}
