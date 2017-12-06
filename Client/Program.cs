daaa090ede8af04b84a5ffc41086605c4443e29a
zhouyu <zhouyu@devzhou.cn>
2017-12-06 11:24:02 +0800

    add multiple port support

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,7 +8,9 @@ namespace Client
   {
     private static void Main(string[] args)
     {
-      var chnanel = new Channel("localhost:8080", ChannelCredentials.Insecure);
+      const int PORT = 8080;
+      var port = args.Length == 0 ? PORT : int.Parse(args[0]);
+      var chnanel = new Channel($"localhost:{port}", ChannelCredentials.Insecure);
       var client = new UserService.UserServiceClient(chnanel);
       var result = client.GetUser(new NotFound());
       Console.WriteLine(result.ToString());
